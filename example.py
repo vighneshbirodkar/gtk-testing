d288c87e9f8c3cf51351745b87aeb6c7f5bfd168
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2013-07-12 02:08:13 +0530

    blah

--- example.py
+++ example.py
@@ -20,17 +20,15 @@ class Base:
         self.c1.scale (512, 512)
         self.c2.scale (512, 512)
         
-        self.c1.move_to(0,0)
-        self.c1.line_to(.5,.5)
-        self.c1.set_source_rgb (0.3, 0.2, 0.5) # Solid color
-        self.c1.set_line_width (0.02)
-        self.c1.stroke()
+
         
-        self.ims1.write_to_png ("example.png")
+        #self.ims1.write_to_png ("example.png")
         
         self.img.set_from_file("lenna.png")
 
         self.window.add(self.img)
+        
+        
         self.window.show_all()
         
         self.img.connect("expose-event", self._expose)
@@ -40,7 +38,15 @@ class Base:
         #w, h = self.allocation.width, self.allocation.height
         cr.set_source_surface(self.ims1)
         cr.paint()
-
+        
+        
+    
+    def drawLine(self):
+        self.c1.move_to(0,0)
+        self.c1.line_to(.5,.5)
+        self.c1.set_source_rgb (0.3, 0.2, 0.5) # Solid color
+        self.c1.set_line_width (0.02)
+        self.c1.stroke()
         
         return True
         
@@ -51,4 +57,5 @@ class Base:
 
 if __name__ == "__main__":
     base = Base()
+    base.drawLine()
     base.main()
